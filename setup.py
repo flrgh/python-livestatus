37da932154976476431bdfc86969053808401324
Michael Martin <michael.martin@dreamhost.com>
2014-12-12 19:01:11 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python-livestatus',
+        version='0.1',
+        description='A Python library for the Livestatus API',
+        url='http://github.com/michaeljmartin/python-livestatus',
+        author='Michael Martin',
+        author_email='michael.joe.martin@gmail.com',
+        license='N/A',
+        packages=['livestatus'],
+        zip_safe=False)
